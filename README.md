8b9f03cfc5c29a948a1582a2947e86722821ae5a
Gábor Ember <Gabor.Ember@wup.hu>
2020-10-02 17:54:38 +0200

    Haxchi and Mocha downloading

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# wiiucfwdownloader
+# wiiucfwdownloader - Wii U Custom Firmware Downloader
+This script was created to simplify the SD card preparation process of installing a Custom Firmware on your Wii U. Please use the guide at [https://wiiu.hacks.guide](url) and follow it step by step. When you get to the SD Preparation step just run the wiiu_cfw.sh and select the CFW you want ('h' for Haxchi and 'm' for Mocha). The script will download all required files and create the folder structure that you can simply copy to your SD card.
+
+# Requirements
+- bash
+- wget
+- unzip
+
+# Usage
+1. Run the script `./wiiu_cfw.sh`
+1. Enter the CFW you want to install (`h` for Haxchi and `m` for Mocha)
+1. Wait for the process to finish
+1. Copy the contents of the `sdcard/` folder (not the folder itself) to your SD card
+1. Enjoy
---
+++ wiiu_cfw.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+extractFiles(){
+  mkdir sdcard/
+  mkdir sdcard/install/
+  unzip -q -o download/wup_installer_gx2.zip -d sdcard
+  unzip -q -o download/nanddumper.zip -d sdcard
+  unzip -q -o download/wiiu-extracttosd.zip -d sdcard
+  unzip -q -o download/homebrew_launcher.v1.4.zip -d sdcard
+  unzip -q -o download/savemii_mod.zip -d sdcard
+  unzip -q -o download/payload.zip -d sdcard/wiiu
+  if [ $cfwtype == 'h' ]
+  then
+    unzip -q -o download/homebrew_launcher_channel.v2.1_fix.zip -d sdcard/install
+    unzip -q -o download/haxchi.zip -d sdcard
+  else
+    unzip -q -o download/mocha.zip -d sdcard
+    copy download/config.ini sdcard/wiiu/apps/mocha/
+  fi
+}
+
+downloadFiles(){
+  if [ $cfwtype == 'h' ]
+  then
+    wget -i haxchi_dl.txt -c -q -P download/
+  else
+    wget -i mocha_dl.txt -c -q -P download/
+  fi
+  extractFiles
+}
+
+askCFW(){
+  echo 'Choose CFW (h - Haxchi, m - Mocha)'
+  read cfwtype
+  if [ $cfwtype == 'h' ]
+  then
+    echo 'Downloading files for Haxchi'
+  else
+    if [ $cfwtype == 'm' ]
+    then
+      echo 'Downloading files for Mocha'
+    else
+      echo 'Please type h or m!'
+      askCFW
+    fi
+  fi
+  downloadFiles
+}
+
+askCFW
